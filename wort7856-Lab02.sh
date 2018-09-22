e243f75244bf34b07af24f5eefd27fe7ace9f0ad
Anne Worthington <wort7856@stthomas.edu>
2018-09-22 01:38:16 +0000

    2nd script commit

--- wort7856-Lab02.sh
+++ wort7856-Lab02.sh
@@ -4,7 +4,7 @@
 # Put commands below
 # To run, type ./YourID-Lab02.sh (you need the "./")
 # It must have permissions starting with a "7"
-mkdir -p ~/main/{A/{B,C,D},C/{E,F},D/{G,H/{J,K},I}}
-touch ~/main/{A/{aa,B/bb,C/cc,D/dd},C/{cc,E/ee,F/ff},D/{dd,G/gg,H/{hh,J/jj,K/kk},I/ii}}
+mkdir -p ~/main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
+touch ~/main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
 apt-get -y install tree
 apt-get -y install git

07662ca5327b99778e6985706f6a2a4dc9bf4355
Anne Worthington <wort7856@stthomas.edu>
2018-09-22 01:00:51 +0000

    script commit

---
+++ wort7856-Lab02.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Starter shell script
+# Rename as YourID-Lab02.sh
+# Put commands below
+# To run, type ./YourID-Lab02.sh (you need the "./")
+# It must have permissions starting with a "7"
+mkdir -p ~/main/{A/{B,C,D},E/{F,G},H/{I,J/{K,L},M}}
+touch ~/main/{A/{aa,B/bb,C/cc,D/dd},E/{ee,F/ff,G/gg},H/{hh,I/ii,J/{jj,K/kk,L/ll},M/mm}}
+apt-get -y install tree
+apt-get -y install git
